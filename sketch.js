47f7f76aff6a9c04bb3b3202e3b2f9b265957c4f
Yong Su <jean.timex@gmail.com>
2022-12-18 22:13:54 -0800

    get rid of p5.js

--- sketch.js
+++ sketch.js
@@ -19,67 +19,98 @@ let generation = 1;
 
 let generationSpan;
 
-function preload() {
-	birdImg = loadImage("assets/bird.png");
-	pipeTopImg = loadImage("assets/pipeUp.png");
-	pipeDownImg = loadImage("assets/pipeDown.png");
-	bg = loadImage("assets/bg.png");
-	groundImg = loadImage("assets/ground.png");
+let context;
+
+async function init() {
+  await preload();
+  setup();
+  draw();
+}
+
+function loop() {
+  requestAnimationFrame(() => {
+    draw();
+    loop();
+  });
+}
+
+window.addEventListener("load", () => {
+  init();
+});
+
+async function preload() {
+  birdImg = await loadImage("assets/bird.png");
+  pipeTopImg = await loadImage("assets/pipeUp.png");
+  pipeDownImg = await loadImage("assets/pipeDown.png");
+  bg = await loadImage("assets/bg.png");
+  groundImg = await loadImage("assets/ground.png");
 }
 
 function setup() {
-	let canvas = createCanvas(bg.width, bg.height);
-	generationSpan = select("#generation");
-	generationSpan.html(generation);
-	canvas.parent("sketch");
-	for (let i = 0; i < totalPopulation; i++) {
-		let bird = new Bird();
-		aliveBirds[i] = bird;
-		allBirds[i] = bird;
-	}
+  width = bg.width;
+  height = bg.height;
+
+  canvas = createCanvas(bg.width, bg.height);
+  context = canvas.getContext("2d");
+
+  generationSpan = document.querySelector("#generation");
+  generationSpan.textContext = generation;
+
+  const sketchDiv = document.getElementById("sketch");
+  sketchDiv.appendChild(canvas);
+  for (let i = 0; i < totalPopulation; i++) {
+    let bird = new Bird();
+    aliveBirds[i] = bird;
+    allBirds[i] = bird;
+  }
 }
 
 function draw() {
-	image(bg, 0, 0);
-	for (let i = pipes.length - 1; i >= 0; i--) {
-		pipes[i].update();
-		if (pipes[i].checkOffScreen()) {
-			pipes.splice(i, 1);
-		}
-	}
+  image(bg, 0, 0);
 
-	for (let i = aliveBirds.length - 1; i >= 0; i--) {
-		let bird = aliveBirds[i];
-		bird.chooseAction(pipes);
-		bird.update();
-		for (let j = 0; j < pipes.length; j++) {
-			if (pipes[j].checkCollision(bird)) {
-				aliveBirds.splice(i, 1);
-				break;
-			}
-		}
-		if (bird.bottomTopCollision()) {
-			aliveBirds.splice(i, 1);
-		}
-	}
+  for (let i = pipes.length - 1; i >= 0; i--) {
+    pipes[i].update();
+    if (pipes[i].checkOffScreen()) {
+      pipes.splice(i, 1);
+    }
+  }
 
-	if (frameCounter % 50 === 0) {
-		pipes.push(new Pipe());
-	}
+  for (let i = aliveBirds.length - 1; i >= 0; i--) {
+    let bird = aliveBirds[i];
+    bird.chooseAction(pipes);
+    bird.update();
+    for (let j = 0; j < pipes.length; j++) {
+      if (pipes[j].checkCollision(bird)) {
+        aliveBirds.splice(i, 1);
+        break;
+      }
+    }
+    if (bird.bottomTopCollision()) {
+      aliveBirds.splice(i, 1);
+    }
+  }
 
-	frameCounter++;
+  if (frameCounter % 50 === 0) {
+    pipes.push(new Pipe());
+  }
 
-	for (let i = 0; i < pipes.length; i++) {
-		pipes[i].show();
-	}
-	for (let i = 0; i < aliveBirds.length; i++) {
-		aliveBirds[i].show();
-	}
-	if (aliveBirds.length == 0) {
-		generation++;
-		generationSpan.html(generation);
-		createNextGeneration();
-	}
+  frameCounter++;
 
-	image(groundImg, 0, height - groundImg.height);
+  for (let i = 0; i < pipes.length; i++) {
+    pipes[i].show();
+  }
+  for (let i = 0; i < aliveBirds.length; i++) {
+    aliveBirds[i].show();
+  }
+  if (aliveBirds.length == 0) {
+    generation++;
+    generationSpan.textContext = generation;
+    createNextGeneration();
+  }
+
+  image(groundImg, 0, height - groundImg.height);
+
+  requestAnimationFrame(() => {
+    draw();
+  });
 }
